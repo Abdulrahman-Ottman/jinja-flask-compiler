f1ec692c4919a6dc6bbcd132fdcd6c1ea97872c8
unknown <abdulrahman.othman.sy@gmail.com>
2025-12-22 14:53:29 +0300

    creating SybmolsTable data structure

---
+++ src/SymbolsTable/SymbolsTable.java
@@ -0,0 +1,28 @@
+package SymbolsTable;
+
+import java.util.LinkedHashMap;
+
+import java.util.Map;
+
+public class SymbolsTable {
+    private static SymbolsTable instance;
+    private final Map<String, Map<String, Object>> symbols = new LinkedHashMap<>();
+
+    private SymbolsTable() {}
+
+    public static SymbolsTable getInstance() {
+        if (instance == null) {
+            instance = new SymbolsTable();
+        }
+        return instance;
+    }
+
+    public void addSymbol(String key, Map<String, Object> value) {
+        symbols.put(key, value);
+    }
+
+    public Map<String, Object> getSymbol(String key) {
+        return symbols.get(key);
+    }
+
+}
